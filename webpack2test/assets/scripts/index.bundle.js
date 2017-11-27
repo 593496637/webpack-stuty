de20f1458ef7d7335f65c7b5e9383310e925cb52
云清淡雅 <593496637@qq.com>
2017-11-27 18:43:55 +0800

    Add files via upload

---
+++ webpack2test/assets/scripts/index.bundle.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(0);
+var data = 213;
+console.log(data);
+
+/***/ })
+],[1]);
--- webpack2test/webpack.config.js
+++ webpack2test/webpack.config.js
@@ -1,33 +1,52 @@
-const path =require("path");
-const ExtractTextPlugin =require("extract-text-webpack-plugin");
-module.exports={
-    entry:{
-        'index':'./assets/scripts/index.es'
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin=require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    entry: {
+        'index': './assets/scripts/index.es'
     },
-    output:{
-        path:path.join(__dirname,'./assets'),
-        publicPath:'./',
-        filename:'scripts/[name].bundle.js'
+    output: {
+        path: path.join(__dirname, './assets'),
+        publicPath: './',
+        filename: 'scripts/[name].bundle.js'
     },
-    module:{
-        rules:[{
-            test : /\.es/,
-            use:[{
-                loader:'babel-loader',
-                options:{
-                    "presets":["es2015","stage-0"]
-                }
-            },{
-                test:/\.less$/i,
-                use:ExtractTextPlugin.extract({
-                    fallback:"style-loader",
-                    use:[{
-                        loader:"css-loader"
-                    },{
-                        loader:"less-loader"
-                    }]
+    module: {
+        rules: [{
+                test: /\.es$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        "presets": ["es2015", "stage-0"]
+                    }
+                }]
+            },
+            {
+                test: /\.less$/i,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'less-loader']
                 })
-            }]
-        }]
-    }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("style/[name].css"),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "commons",
+            // ( 公共chunk(commnons chunk) 的名称)
+          
+            filename: "scripts/[name].js",
+            // ( 公共chunk 的文件名)
+          
+            minChunks: 2
+            // (模块必须被3个 入口chunk 共享)
+          
+            // chunks: ["pageA", "pageB"],
+            // (只使用这些 入口chunk)
+        }),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/assets/index.html"//new 一个这个插件的实例，并传入相关的参数
+        })
+    ]
 }
